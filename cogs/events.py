bb14f8b1db76bb02f5f79a28b7dbce58d1db2962
NextChai <75498301+NextChai@users.noreply.github.com>
2021-05-09 23:00:19 -0400

    remove temp command

--- cogs/events.py
+++ cogs/events.py
@@ -32,10 +32,6 @@ class Events(commands.Cog):
         self.extractor = URLExtract()
         self.extractor.update()
         
-    @commands.command()
-    async def temp(self, ctx):
-        return await ctx.send(len(profanity.CENSOR_WORDSET))
-        
     def moderator_check(self, author):
         author_roles = [role.id for role in author.roles]
         if BYPASS_FURY in author_roles:
