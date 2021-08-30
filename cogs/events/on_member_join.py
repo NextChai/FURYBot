be28821eb811d3669db75eb5aa10a7f1f6a09ae0
NextChai <75498301+NextChai@users.noreply.github.com>
2021-08-30 12:09:42 -0400

    Update on_member_join.py

--- cogs/events/on_member_join.py
+++ cogs/events/on_member_join.py
@@ -27,11 +27,10 @@ class OnMemberJoin(BaseEvent, command_attrs=dict(hidden=True)):
 
         e = self.bot.Embed()
         e.title = 'Welcome!'
-        e.description = f'Welcome {member.mention}! Check out the info below this line:'
+        e.description = f'Welcome {member.mention}! Check out the info below this line:\n\n`Read all of it!`'
         e.add_field(
             name='Change your nickname!',
-            value='Change your server nickname to your **first name last initial!**\nExample: Trevor F.\n\n' \
-                'We do this so it is easy for the Coaches to keep track of ya.')
+            value='Change your server nickname to your **first name last initial!**\nExample: Trevor F.')
         e.add_field(
             name="Don't know how to change your nickname?",
             value=f'Type any message in the {channel.mention} chat, right click your name, and select "Change Nickname."')
@@ -44,13 +43,13 @@ class OnMemberJoin(BaseEvent, command_attrs=dict(hidden=True)):
         
         e.add_field(
             name='eSports Meetings!',
-            value=f'If you are 13+ years of age we have eSports meetings this Thursday the 2nd and Friday the 3rd.' \
+            value=f'If you are 13+ years of age we have eSports meetings this Thursday the 2nd and Friday the 3rd.\n\n' \
                 f'**Thursday:**\n**{discord.utils.format_dt(thursday)}:** Quick meeting about info with eSports.\n\n' \
                     '**Friday:**\n' \
                     f'**{discord.utils.format_dt(four)} (30 mins long)** Super Smash Players' \
                     f'\n**{discord.utils.format_dt(fourthirty)} (30 mins long)** League of Legends Players' \
-                    f'\n**{discord.utils.format_dt(five)}** (1 hour long) Overwatch and Fortnite players' \
-                    f'\n**{discord.utils.format_dt(six)}:** (30 mins long) Rocket League Players',
+                    f'\n**{discord.utils.format_dt(five)}** (1 hour long)** Overwatch and Fortnite players' \
+                    f'\n**{discord.utils.format_dt(six)} (30 mins long)** Rocket League Players',
             inline=False
         )
         
