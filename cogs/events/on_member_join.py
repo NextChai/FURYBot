2131e0ee22ebbd95e5e72b19316c92ded6752237
NextChai <75498301+NextChai@users.noreply.github.com>
2021-08-30 12:05:49 -0400

    Update on_member_join.py

--- cogs/events/on_member_join.py
+++ cogs/events/on_member_join.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 from cogs.events.base import BaseEvent
 from cogs.utils.constants import GENERAL_CHANNEL
 
+from datetime import datetime, timedelta
+
 
 def setup(bot):
     bot.add_cog(OnMemberJoin(bot))
@@ -33,6 +35,23 @@ class OnMemberJoin(BaseEvent, command_attrs=dict(hidden=True)):
         e.add_field(
             name="Don't know how to change your nickname?",
             value=f'Type any message in the {channel.mention} chat, right click your name, and select "Change Nickname."')
+        
+        thursday = datetime(year=2021, month=9, day=2, hour=14, minute=0, second=0)
+        four = thursday + timedelta(days=0, hours=23)
+        fourthirty = four + timedelta(minutes=30)
+        five = four + timedelta(hours=1)
+        six = five  + timedelta(hours=1)
+        
+        e.add_field(
+            name='eSports Meetings!',
+            value=f'If you are 13+ years of age we have eSports meetings this Thursday the 2nd and Friday the 3rd.' \
+                f'**Thursday:**\n**{discord.utils.format_dt(thursday)}:** Quick meeting about info with eSports.\n\n' \
+                    '**Friday:**\n' \
+                    f'**{discord.utils.format_dt(four)} (30 mins long)** Super Smash Players' \
+                    f'\n**{discord.utils.format_dt(fourthirty)} (30 mins long)** League of Legends Players' \
+                    f'\n**{discord.utils.format_dt(five)}** (1 hour long) Overwatch and Fortnite players' \
+                    f'\n**{discord.utils.format_dt(six)}:** (30 mins long) Rocket League Players'
+        )
         
         try:
             await member.send(embed=e)
