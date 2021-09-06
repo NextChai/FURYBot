0cb986b6c3671a891bbc3ccf49f0e21808572bbe
NextChai <75498301+NextChai@users.noreply.github.com>
2021-09-06 13:15:11 -0400

    Updoots

--- cogs/owner.py
+++ cogs/owner.py
@@ -7,35 +7,7 @@ class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.slash(description="See any errors found inside of the bot.")
-    @commands.is_owner()
-    async def errors(self, ctx):
-        if not hasattr(self.bot, 'command_errors'):
-            return await ctx.send("No errors to show.")
-        
-        errors = []
-        for command in self.bot.command_errrors:
-            command = self.bot.command_errors[command]
-            if command.get("count") > 0 :
-                errors.append(command)
-            
-        if not errors:
-            return await ctx.send("No errors to show.") # this technically can't happen, but its chill
-        
-        e = discord.Embed(color=discord.Color.blue())
-        for error in errors:
-            jumps = ', '.join(error.get('jump'))
-            e.add_field(name=error.get('name'), value=f"**Count:** {error.get('count')}\nJump: {jumps}\nTraceback: {error.get('traceback')[0]}")
-        await ctx.send(embed=e)
-        
-    @commands.slash(description="Reset the errors found inside of the bot.")
-    @commands.is_owner()
-    async def reset_errors(self, ctx):
-        if not hasattr(self.bot, "command_errors"):
-            return await ctx.send("command_errors has not been set yet, you're all good.")
-        
-        self.bot.command_errors = {}
-        return await ctx.send("command_errors has been reset, you're all good to go.")
+    # Note: nothing here yet due to new update
     
 
     
