8db169a14192e3b47fc3526b968677d10e26991e
NextChai <futureengineerdlc@gmail.com>
2021-09-14 19:49:11 -0400

    Ok remove eval command for now

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,12 +1,10 @@
 import discord
 from discord.ext import commands
 
-from contextlib import redirect_stdout
 import sys
 import importlib
 import re
 import os
-import io
 import textwrap
 import traceback
 import asyncio
@@ -148,60 +146,6 @@ class Owner(commands.Cog):
             return await ctx.send(lines)
         return await ctx.send(f'Loaded {extension} sucessfully.')
 
-    def cleanup_code(self, content):
-        """Automatically removes code blocks from the code."""
-        # remove ```py\n```
-        if content.startswith('```python') or content.startswith('```py') and content.endswith('```'):
-            return '\n'.join(content.split('\n')[1:-1])
-
-        # remove `foo`
-        return content.strip('` \n')
-
-    @jsk.slash()
-    @commands.is_owner()
-    async def python(self, ctx, code_block: str):
-        clean = self.cleanup_code(code_block)
-        
-        env = {
-            'bot': self.bot,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-        }
-
-        env.update(globals())
-
-        stdout = io.StringIO()
-        to_compile = f'async def func():\n{textwrap.indent(clean, "  ")}'
-
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                self._last_result = ret
-                await ctx.send(f'```py\n{value}{ret}\n```')
-        
-    
     @commands.slash()
     @commands.has_permissions(manage_channels=True)
     async def createteam(
