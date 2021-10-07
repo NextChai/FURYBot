dd359ed5833ec70ee153a85b533414a85be7706e
NextChai <75498301+NextChai@users.noreply.github.com>
2021-10-07 03:03:43 -0400

    Debooging

--- bot.py
+++ bot.py
@@ -259,7 +259,7 @@ class Security(CustomProfanity, URLExtract):
         """
         links = await self.wrap(self.gen_urls, message)
         if not links:
-            return re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
+            links = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message)
         return links
     
     async def contains_links(self, message: str) -> bool:
