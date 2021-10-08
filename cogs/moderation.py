5624739a6b3a7e1e127c896272c771223cd2c0cd
NextChai <75498301+NextChai@users.noreply.github.com>
2021-10-07 23:19:35 -0400

    Take r.danny code

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,5 +1,10 @@
 from __future__ import annotations
 
+import os
+import re
+import sys
+import importlib
+
 import discord
 from discord.ext import commands
 
@@ -16,6 +21,80 @@ if TYPE_CHECKING:
 class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot: FuryBot = bot
+        self._GIT_PULL_REGEX = re.compile(r'\s*(?P<filename>.+?)\s*\|\s*[0-9]+\s*[+-]+')
+        
+    def find_modules_from_git(self, output):
+        files = self._GIT_PULL_REGEX.findall(output)
+        ret = []
+        for file in files:
+            root, ext = os.path.splitext(file)
+            if ext != '.py':
+                continue
+
+            if root.startswith('cogs/'):
+                # A submodule is a directory inside the main cog directory for
+                # my purposes
+                ret.append((root.count('/') - 1, root.replace('/', '.')))
+
+        # For reload order, the submodules should be reloaded first
+        ret.sort(reverse=True)
+        return ret
+
+    def reload_or_load_extension(self, module):
+        try:
+            self.bot.reload_extension(module)
+        except commands.ExtensionNotLoaded:
+            self.bot.load_extension(module)
+            
+    @commands.group(name='git')
+    async def git(self) -> None:
+        pass
+    
+    @git.slash(
+        name='pull',
+        description='Pull from the github to update the bot'
+    )
+    async def git_pull(self, ctx) -> None:
+        async with ctx.typing():
+            stdout, stderr = await self.run_process('git pull')
+
+        # progress and stuff is redirected to stderr in git pull
+        # however, things like "fast forward" and files
+        # along with the text "already up-to-date" are in stdout
+
+        if stdout.lower().startswith('already'):
+            return await ctx.send(stdout)
+
+        modules = self.find_modules_from_git(stdout)
+        mods_text = '\n'.join(f'{index}. `{module}`' for index, (_, module) in enumerate(modules, start=1))
+        prompt_text = f'This will update the following modules, are you sure?\n{mods_text}'
+        confirm = await ctx.get_confirmation(prompt_text)
+        if not confirm:
+            return 
+
+        statuses = []
+        for is_submodule, module in modules:
+            if is_submodule:
+                try:
+                    actual_module = sys.modules[module]
+                except KeyError:
+                    statuses.append((ctx.tick(None), module))
+                else:
+                    try:
+                        importlib.reload(actual_module)
+                    except Exception as e:
+                        statuses.append((ctx.tick(False), module))
+                    else:
+                        statuses.append((ctx.tick(True), module))
+            else:
+                try:
+                    self.reload_or_load_extension(module)
+                except commands.ExtensionError:
+                    statuses.append((ctx.tick(False), module))
+                else:
+                    statuses.append((ctx.tick(True), module))
+
+        await ctx.send('\n'.join(f'{status}: `{module}`' for status, module in statuses))
 
     @commands.group(name='profanity', description='Handle the profanity filter')
     @commands.check_any(is_captain(), is_coach(), is_mod())
