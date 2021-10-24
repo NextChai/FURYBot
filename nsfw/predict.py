aa525c90333c05258eb3a63906314b82234b95bd
NextChai <75498301+NextChai@users.noreply.github.com>
2021-10-24 00:27:20 -0400

    Trying something

--- nsfw/predict.py
+++ nsfw/predict.py
@@ -68,6 +68,7 @@ def load_images(image_paths, image_size, verbose=True):
         except Exception as ex:
             print("Image Load Failure: ", img_path, ex)
 
+    return np.array([loaded_images]), loaded_image_paths
     return np.asarray(loaded_images), loaded_image_paths
 
 
