268950eaa0afe9aecce974739d6466fb92ea2d6a
NextChai <75498301+NextChai@users.noreply.github.com>
2021-11-29 01:59:23 -0500

    Fix sql error

--- cogs/utils/timer.py
+++ cogs/utils/timer.py
@@ -70,7 +70,7 @@ class TimerHandler:
             await ctx.send(f'You called the {ctx.command.name} command with too many arguments.')
             
     async def get_active_timer(self, *, connection=None, days=7):
-        query = f"SELECT * FROM {self.name} WHERE (expires IS NOT NULL AND expires < (CURRENT_DATE + $1::interval)) AND dispatched IS $2 ORDER BY expires LIMIT 1;"
+        query = f"SELECT * FROM {self.name} WHERE (expires IS NOT NULL AND expires < (CURRENT_DATE + $1::interval)) AND dispatched = $2 ORDER BY expires LIMIT 1;"
         con = connection or self.bot.pool
 
         record = await con.fetchrow(query, datetime.timedelta(days=days), False)
