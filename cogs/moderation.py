5e51d20b99166b9de0516d55491f851625d88fb9
NextChai <75498301+NextChai@users.noreply.github.com>
2021-11-28 20:00:59 -0500

    Fix attr issue

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -295,14 +295,21 @@ class Moderation(commands.Cog):
         description='Give a sub access to a channel.'
     )
     @commands.describe('member', description='The member to give access to.')
-    @commands.describe('channel', channels=[commands.ChannelType.text, commands.ChannelType.voice])
+    @commands.describe(
+        'channel', 
+        type=commands.OptionType.channel,
+        channels=[
+            commands.ChannelType.text, 
+            commands.ChannelType.voice
+        ]
+    )
     @commands.describe('permission', description='Whether to allow or deny access.')
     @commands.check_any(is_captain(), is_coach(), is_mod())
     async def sub(
         self, 
         ctx, 
         member: discord.Member, 
-        channel: discord.TextChannel, 
+        channel: str, 
         permission: bool
     ) -> None:
         value = True if permission == 'allow' else False
