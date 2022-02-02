fe31a47d0d1f0d01164f4ebd5bbce09488bcf38c
NextChai <75498301+NextChai@users.noreply.github.com>
2022-02-02 17:17:14 -0500

    Fix lockdown command

--- bot.py
+++ bot.py
@@ -617,7 +617,7 @@ class Lockdown:
                     await channel.edit(overwrites=overwrites)
                     channels.append(channel.id)
         
-        roles = [r.id for r in member.roles]
+        roles = [r.id for r in member.roles if r.is_assignable()]
         
         new_kwargs['channels'] = channels
         new_kwargs['roles'] = roles
