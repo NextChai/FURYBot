5a549138bb409ecc5ec6a7383d2704124981a789
NextChai <75498301+NextChai@users.noreply.github.com>
2022-02-01 20:19:25 -0500

    Add documentation

--- cogs/utils/__init__.py
+++ cogs/utils/__init__.py
@@ -12,7 +12,21 @@ from inspect import signature
 T = TypeVar('T')
 
 def copy_doc(original: Callable) -> Callable[[T], T]:
+    """Used to copy the documenation from one function to another.
+    
+    Parameters
+    ----------
+    original: Callable
+        The original function to copy the documenation from.
+    """
     def decorator(overriden: T) -> T:
+        """Used to transfer the documentation from one function to another.
+        
+        Parameters
+        ----------
+        overridden: Callable   
+            The function to copy the documentation to.
+        """
         if overriden.__doc__:
             overriden.__doc__ = f'{overriden.__doc__}\n{original.__doc__}'
         else:
