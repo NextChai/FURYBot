b140028597efa06b6ec07a424bdfd5089cb38576
NextChai <75498301+NextChai@users.noreply.github.com>
2022-05-28 12:21:03 -0400

    Update owner.py for the new pagintor changes

--- cogs/owner.py
+++ cogs/owner.py
@@ -25,34 +25,54 @@ from __future__ import annotations
 
 import re
 import sys
-import importlib
 import importlib.util
 import logging
-from typing import (
-    TYPE_CHECKING,
-    Optional,
-    List,
-)
-from typing_extensions import Self
+from typing import TYPE_CHECKING, Any, List, Sequence, Optional
 
 import discord
 from discord.ext import commands
 
-from jishaku.shell import ShellReader # type: ignore
-
 from utils import BaseCog
 from utils.context import Context, tick
-from utils.paginator import AsyncCodePaginator
+from utils.paginator import Paginator, PaginatorView
+from utils.shell import AsyncShellExecutor
 
 if TYPE_CHECKING:
     from bot import FuryBot
 
 log = logging.getLogger(__name__)
 
-_GIT_PULL_REGEX: re.Pattern[str] = re.compile(r'(?P<path>(?:[a-z]{1,}/){1,})(?P<filename>[a-z]{1,}).py')
+GIT_PULL_REGEX: re.Pattern[str] = re.compile(r'(?P<path>(?:[a-z]{1,}/){1,})(?P<filename>[a-z]{1,}).py')
+GIT_REPO_REGEX: re.Pattern[str] = re.compile(
+    r'https://github.com/(?P<owner>[\w\-\.]+)/(?P<repo>[\w\-\.]+)/([\w\-\.]+)/(?P<branch>[\w\-]+)(?P<path>(?:/[\w\.\-]{1,}){1,})(?:#L(?P<opening>[0-9]+)(?:\-L(?P<closing>[0-9]+))?)?',
+    flags=re.IGNORECASE,
+)
 
 
-class GitPullSelect(discord.ui.Select['GitPull']):
+async def _reload_extension(bot: FuryBot, extension: str) -> str:
+    if extension not in bot.extensions:
+        try:
+            spec = importlib.util.find_spec(extension)
+        except ModuleNotFoundError:
+            return tick(None, extension)
+
+        if spec is None:
+            return tick(None, extension)
+
+        module = importlib.util.module_from_spec(spec)
+        sys.modules[extension] = module
+        return tick(True, extension)
+    else:
+        try:
+            await bot.reload_extension(extension)
+        except Exception as exc:
+            log.warning(f'Failed to reload extension {extension}.', exc_info=exc)
+            return tick(False, f'{extension}: {exc}')
+
+        return tick(True, extension)
+
+
+class GitPullSelect(discord.ui.Select['GitPullSelect']):
     """
     A select used by the owner to choose extensions to reload.
 
@@ -64,9 +84,9 @@ class GitPullSelect(discord.ui.Select['GitPull']):
         The parent GitPull cog.
     """
 
-    def __init__(self, parent: GitPull, /, *, extensions: List[str]) -> None:
-        self.extensions: List[str] = extensions
-        self.parent: GitPull = parent
+    def __init__(self, bot: FuryBot, /, *, extensions: Sequence[str], **kwargs: Any) -> None:
+        self.bot: FuryBot = bot
+        self.extensions: Sequence[str] = extensions
         super().__init__(
             placeholder='Select an extension(s) to reload...',
             options=[
@@ -77,6 +97,7 @@ class GitPullSelect(discord.ui.Select['GitPull']):
                 for extension in extensions
             ],
             max_values=len(extensions),
+            **kwargs,
         )
 
     async def callback(self, interaction: discord.Interaction) -> None:
@@ -90,103 +111,83 @@ class GitPullSelect(discord.ui.Select['GitPull']):
             The interaction that was created by this select.
         """
         statuses: List[str] = []
-        for value in self.values:
-            statuses.append(await self.parent._reload_extension(value)) # type: ignore
+        for extension in self.extensions:
+            statuses.append(await self.parent._reload_extension(extension))  # type: ignore
 
         await interaction.edit_original_message(content='Reloaded\n' + '\n'.join(statuses), view=None)
 
 
-class GitPull(discord.ui.View):
-    """
-    A view to allow the owner to reload extensions after
-    pulling from github.
-
-    Attributes
-    ----------
-    extensions: List[:class:`str`]
-        A list of extensions to choose from.
-    ctx: :class:`Context`
-        The invocation context of the command.
-    bot: :class:`FuryBot`
-        The bot instance.
-    """
-
-    def __init__(self, extensions: List[str], /, *, ctx: Context) -> None:
-        super().__init__(timeout=120)
-
+class ReloadAll(discord.ui.Button[Any]):
+    def __init__(self, bot: FuryBot, *, extensions: List[str], **kwargs: Any) -> None:
+        self.bot: FuryBot = bot
         self.extensions: List[str] = extensions
-        self.ctx: Context = ctx
-        self.bot: FuryBot = ctx.bot
+        super().__init__(label='Reload All', style=discord.ButtonStyle.green, **kwargs)
 
-        for extension_packet in self.bot.chunker(extensions, size=20):  # type: ignore
-            extension_packet: List[str]
-
-            self.add_item(GitPullSelect(self, extensions=extension_packet))
-
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        """|coro|
-
-        A check called before the interaction is processed.
-
-        Parameters
-        ----------
-        interaction: :class:`discord.Interaction`
-            The interaction that is being processed.
-
-        Returns
-        -------
-        :class:`bool`
-            Whether or not the interaction should be processed.
-        """
-        result = interaction.user == self.ctx.author
-        if not result:
-            await interaction.response.send_message('Hey! You can\'t do that!')
-
-        return result
-
-    async def _reload_extension(self, extension: str) -> str:
-        if extension not in self.bot.extensions:
-            try:
-                spec = importlib.util.find_spec(extension)
-            except ModuleNotFoundError:
-                return tick(None, extension)
-
-            if spec is None:
-                return tick(None, extension)
-
-            module = importlib.util.module_from_spec(spec)
-            sys.modules[extension] = module
-            return tick(True, extension)
-        else:
-            try:
-                await self.bot.reload_extension(extension)
-            except Exception as exc:
-                log.warning(f'Failed to reload extension {extension}.', exc_info=exc)
-                return tick(False, f'{extension}: {exc}')
-
-            return tick(True, extension)
-
-    @discord.ui.button(label='Reload All', style=discord.ButtonStyle.green)
-    async def reload_all(self, interaction: discord.Interaction, button: discord.ui.Button[Self]) -> None:
-        """|coro|
-
-        A button used to reload all extensions instead of using the select.
-
-        Parameters
-        ----------
-        interaction: :class:`discord.Interaction`
-            The interaction that was created by this button.
-        button: :class:`discord.ui.Button`
-            The button that was pressed.
-        """
+    async def callback(self, interaction: discord.Interaction) -> Any:
         statuses: List[str] = []
         for extension in self.extensions:
-            statuses.append(await self._reload_extension(extension))
+            statuses.append(await _reload_extension(self.bot, extension))
 
         await interaction.response.edit_message(content='Reloaded\n' + '\n'.join(statuses), view=None)
 
 
-class Owner(BaseCog, brief='The Owner commands.', emoji='\N{BLACK SUN WITH RAYS}'):
+class Github(BaseCog):
+    def __init__(self, bot: FuryBot) -> None:
+        super().__init__(bot)
+
+    @commands.group(
+        name='git',
+        brief='Perform github actions and use the github API.',
+        description='Perform github actions and use the github API.',
+        aliases=['github'],
+    )
+    async def github(self, ctx: Context, *, argument: str = 'pull') -> Optional[discord.Message]:
+        if ctx.invoked_subcommand:
+            return
+
+    @github.command(
+        name='pull',
+        brief='Pull from github.',
+        description='Pull from github.',
+    )
+    async def github_pull(self, ctx: Context) -> Optional[discord.Message]:
+        """|coro|
+
+        Used to perform a git operation and return the output. This is a
+        special command, in which if `git pull` is called it will display
+        a view to the user to reload any extensions.
+        """
+        await ctx.typing()
+
+        paginator = Paginator(prefix='```python')
+        buffer = ''
+
+        async with AsyncShellExecutor(f'git pull') as reader:  # type: ignore
+            line: Optional[str]
+            async for line in reader:
+                buffer += f'{line}\n'
+
+        paginator.add_line(buffer)
+
+        extensions: List[str] = []
+        matches = GIT_PULL_REGEX.findall(buffer)
+        if matches:
+            extensions = [path.replace('/', '.') + filename for (path, filename) in matches]
+            paginator.prefix = 'Extensions\n' + '\n'.join(f'- {extension}' for extension in extensions) + '\n```python'
+        else:
+            paginator.prefix = f'No matches found from github regex.\n```python'
+
+        view = PaginatorView(paginator, timeout=180, author=ctx.author)
+        if matches:
+            for extension_packet in self.bot.chunker(extensions, size=20):
+                view.add_item(GitPullSelect(self.bot, extensions=extension_packet))  # type: ignore
+
+            view.add_item(ReloadAll(self.bot, extensions=extensions))
+
+        await ctx.send(content=paginator.pages[0], view=view)
+
+
+class Owner(Github, brief='The Owner commands.', emoji='\N{BLACK SUN WITH RAYS}'):
     """
     The main owner cog. This cog contains commands
     that are only usable by the bot owner.
@@ -195,41 +196,7 @@ class Owner(BaseCog, brief='The Owner commands.', emoji='\N{BLACK SUN WITH RAYS}
     async def cog_check(self, ctx: Context) -> bool: # type: ignore
         return await self.bot.is_owner(ctx.author)
 
-    async def git_pull_optimization(self, ctx: Context, buffer: str) -> Optional[discord.Message]:
-        matches = _GIT_PULL_REGEX.findall(buffer)
-        if not matches:
-            return await ctx.send(f'No matches inside the `buffer` were found.')
-
-        extensions: List[str] = []
-        for path, filename in matches:
-            path = path.replace('/', '.')
-
-            extensions.append(path + filename)
-
-        view = GitPull(extensions, ctx=ctx)
-        return await ctx.send(content='Extensions\n' + '\n'.join(f'- {extension}' for extension in extensions), view=view)
-
-    @commands.command(name='git')
-    async def git(self, ctx: Context, *, argument: str = 'pull') -> Optional[discord.Message]:
-        """|coro|
-
-        Used to perform a git operation and return the output. This is a
-        special command, in which if `git pull` is called it will display
-        a view to the user to reload any extensions.
-        """
-        message = await ctx.send('Pulling...')
-        await ctx.typing()
-
-        paginator = AsyncCodePaginator(message=message, author=ctx.author, prefix='```py')
-        buffer = ''
-
-        with ShellReader(f'git {argument}', loop=self.bot.loop) as reader: # type: ignore
-            async for line in reader: # type: ignore
-                buffer += f'{line}\n'
-
-        await paginator.add_line(buffer)
-        if argument == 'pull':
-            return await self.git_pull_optimization(ctx, buffer)
+    
 
 
 async def setup(bot: FuryBot) -> None:
