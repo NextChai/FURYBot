844414cb3153181738ee65e827c792e71a189d1b
NextChai <75498301+NextChai@users.noreply.github.com>
2022-09-20 13:48:39 -0400

    Fix edit issue

--- cogs/teams.py
+++ cogs/teams.py
@@ -393,9 +393,7 @@ class Teams(BaseCog):
                 votes=[],
             )
             channel = _find_team_text_channel(interaction.guild, home_team['channels'])
-            view.message = await interaction.edit_original_response(
-                embed=view.embed, view=view, content='@everyone', allowed_mentions=discord.AllowedMentions(everyone=True)
-            )
+            view.message = await interaction.edit_original_response(embed=view.embed, view=view)
 
             data = await connection.fetchrow(
                 'INSERT INTO teams.scrims (home_id, away_id, home_message_id, status, scheduled_for) VALUES($1, $2, $3, $4, $5) RETURNING id;',
