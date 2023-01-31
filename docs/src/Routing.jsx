670094ea06d5eeb50e8c9dec80619253224ff294
NextChai <futureengineerdlc@gmail.com>
2023-01-31 12:17:35 -0500

    Add router basename

--- docs/src/Routing.jsx
+++ docs/src/Routing.jsx
@@ -6,7 +6,7 @@ import { Docs, Home, Layout } from './pages';
 const Routing = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename="/Fury-Bot">
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
