380d0bdde456a1d8166df599f851b35ded338e5e
NextChai <75498301+NextChai@users.noreply.github.com>
2023-02-01 11:11:27 -0500

    Move to HashRouter

--- docs/src/Routing.jsx
+++ docs/src/Routing.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter  } from "react-router-dom";
 import { Docs, Home, Layout } from './pages';
 
 
 const Routing = () => {
   return (
     <div>
-      <BrowserRouter basename='/Fury-Bot'>
+      <HashRouter basename='/Fury-Bot'>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path='/docs' element={<Docs />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter >
     </div>
   )
 }
