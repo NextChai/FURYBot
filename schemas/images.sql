2a52c2313a811a5119c9e57f956e7189cbe0088b
NextChai <75498301+NextChai@users.noreply.github.com>
2024-09-08 17:20:56 -0400

    Refactor images schema and tables

--- schemas/images.sql
+++ schemas/images.sql
@@ -1,6 +1,7 @@
 CREATE SCHEMA IF NOT EXISTS images;
 
 CREATE TABLE IF NOT EXISTS images.request_settings (
+    id SERIAL PRIMARY KEY,
     channel_id BIGINT, -- The ID of the channel to send to.
     guild_id BIGINT, -- The ID of the guild related to
     notification_role_id BIGINT
@@ -8,6 +9,7 @@ CREATE TABLE IF NOT EXISTS images.request_settings (
 
 CREATE TABLE IF NOT EXISTS images.requests (
     id BIGSERIAL PRIMARY KEY,
+    request_settings BIGINT REFERENCES images.request_settings(id) ON DELETE CASCADE,
     created_at TIMESTAMP WITH TIME ZONE,
     attachment_payload JSONB,
     requester_id BIGINT,
