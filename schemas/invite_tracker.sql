36dafee6fe34b842a0822d3c70bc70d85d2bf283
Trevor Flahardy <75498301+trevorflahardy@users.noreply.github.com>
2024-10-12 20:07:24 -0400

    Add invite tracker user logging for invite uses

--- schemas/invite_tracker.sql
+++ schemas/invite_tracker.sql
@@ -10,3 +10,13 @@ CREATE TABLE IF NOT EXISTS invite_tracker.invites (
     uses INT NOT NULL DEFAULT 0, -- The number of times the invite has been used
     UNIQUE(guild_id, invite_code)
 );
+
+-- Keeps track of the users who have used a specific invite. This allows
+-- the bot to track which member has joined using which invite.
+CREATE TABLE IF NOT EXISTS invite_tracker.invite_uses (
+    id SERIAL PRIMARY KEY,
+    invite_id INTEGER REFERENCES invite_tracker.invites(id) ON DELETE CASCADE, -- The invite that was used
+    user_id BIGINT NOT NULL, -- The user who used the invite
+    joined_at TIMESTAMP WITH TIME ZONE, -- The time the user joined
+    UNIQUE(invite_id, user_id)
+);
